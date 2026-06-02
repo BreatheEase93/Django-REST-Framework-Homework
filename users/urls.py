4891c030e784af8749c1ceee7d495fe5979aca4c
Dmitry globa <dima.globa2015@yandex.ru>
2026-06-02 13:15:24 +0800

    feat: add urls for Users

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import (
+    UserCreateAPIView,
+    UserDestroyAPIView,
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="user-register"),
+    path("", UserListAPIView.as_view(), name="user-list"),
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-detail"),
+    path("update/<int:pk>/", UserUpdateAPIView.as_view(), name="user-update"),
+    path("delete/<int:pk>/", UserDestroyAPIView.as_view(), name="user-delete"),
+]
