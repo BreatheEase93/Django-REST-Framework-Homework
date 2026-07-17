5039605b76ef30413231b7c353ae4cd338a59f72
Dmitry globa <dima.globa2015@yandex.ru>
2026-07-17 18:03:12 +0800

    feat(matetials): lessons list and count to CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -11,6 +11,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
